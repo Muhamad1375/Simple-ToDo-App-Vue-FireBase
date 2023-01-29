ff0e5dd0df6d24ad5f4ad734c4f5e81057aa9908
Muhamad1375 <Mohammadali.fallahzade@gmail.com>
2023-01-30 00:01:43 +0330

    firebase setup

---
+++ toDo/src/firebase/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBAqjVIYkM4IoGF1Ev6OyRtS-U9kkvMJtU',
+  authDomain: 'badass-todo-751b6.firebaseapp.com',
+  projectId: 'badass-todo-751b6',
+  storageBucket: 'badass-todo-751b6.appspot.com',
+  messagingSenderId: '589005889705',
+  appId: '1:589005889705:web:722c7d7eb4a3bfb65289b8'
+};
+
+const app = initializeApp(firebaseConfig)
