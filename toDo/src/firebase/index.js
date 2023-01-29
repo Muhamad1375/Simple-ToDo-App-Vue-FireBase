d389ba7062f0b7a772919f9d00577f9dd011a1e5
Muhamad1375 <Mohammadali.fallahzade@gmail.com>
2023-01-30 00:08:46 +0330

    cloud firestore setup

--- toDo/src/firebase/index.js
+++ toDo/src/firebase/index.js
@@ -1,4 +1,6 @@
 import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore';
+
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBAqjVIYkM4IoGF1Ev6OyRtS-U9kkvMJtU',
@@ -10,3 +12,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig)
+const db = getFirestore(app);
+
+export {
+      db
+}
