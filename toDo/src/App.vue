1428457c4c46bc3b10363d1e3a406c5d3f8086f3
Muhamad1375 <Mohammadali.fallahzade@gmail.com>
2023-01-29 23:37:42 +0330

    mark done

--- toDo/src/App.vue
+++ toDo/src/App.vue
@@ -29,17 +29,23 @@
       <div
             v-for="todo in todos"
             class="card mb-5"
+            :class="{ 'has-background-success-light' : todo.done }"
         >
       <div class="card-content">
       <div class="content">
 
             <div class="columns is-mobile is-vcentered">
-                  <div class="column">
+                  <div
+                    class="column"
+                    :class="{ 'has-text-success line-through' : todo.done }">
                         {{ todo.content }}
                   </div>
 
                   <div class="column is-5 has-text-right">
-                        <button class="button is-light">
+                        <button
+                        @click="toggleDone(todo.id)"
+                          class="button"
+                          :class="todo.done ? 'is-success' : 'is-light'">
                               &check;
                         </button>
                         <button
@@ -68,16 +74,16 @@ import { v4 as uuidv4 } from 'uuid'
       todos
 */ 
                   const todos = ref([
-            {
-                  id: 'id1',
-                  content: 'Shave my butt',
-                  done: false
-            },
-            {
-                  id: 'id2',
-                  content: 'wash my butt',
-                  done: false
-            }
+            // {
+            //       id: 'id1',
+            //       content: 'Shave my butt',
+            //       done: false
+            // },
+            // {
+            //       id: 'id2',
+            //       content: 'wash my butt',
+            //       done: true
+            // }
       ])
 
 /*
@@ -105,6 +111,16 @@ const deleteTodo = id => {
 }
 
 
+/*
+ toggleDone
+*/
+
+const toggleDone = id => {
+      const index = todos.value.findIndex(todo => todo.id === id)
+      todos.value[index].done = !todos.value[index].done
+}
+
+
 </script>
 
 
@@ -115,6 +131,9 @@ const deleteTodo = id => {
             max-width: 400px;
             padding: 20px;
             margin: 0 auto;
+      }
+      .line-through {
+            text-decoration: line-through;
       }
 </style>
 
